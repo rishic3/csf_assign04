b702144d4b28e94130c510def2b793818a3d5442
rishic3 <rchand18@jhu.edu>
2022-04-12 14:12:22 -0400

    symbol prints

--- magic.cpp
+++ magic.cpp
@@ -106,7 +106,7 @@ int main(int argc, char **argv) {
     for (int i = 0; i < symbolRange; i++) {
 
         unsigned char *name = symbol_strtab_p + symtab[i].st_name;
-        printf("Symbol %u: name=%s, size=%lx, info=%lx, other=%lx", i, name, symtab[i].st_size, symtab[i].st_info, symtab[i].st_other);
+        printf("Symbol %u: name=%s, size=%lx, info=%lx, other=%lx\n", i, name, symtab[i].st_size, symtab[i].st_info, symtab[i].st_other);
 
     }
 
